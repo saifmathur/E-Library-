0883523330848c72e0a8b0954561ce192b9267c2
saifmathur <saifmathur41@gmail.com>
2020-06-17 03:56:50 +0530

    added seperate style for contact template

--- app.js
+++ app.js
@@ -8,10 +8,26 @@ app.set('views', __dirname + '/views')
 
 app.use(express.static(__dirname + '/assets'))
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+
+app.use('/about', function (req,res){
+    res.render('about')
+})
+
+app.use('/contact',function (req, res){
+    res.render('contact')
+})
 
 app.use('/', function(req,res){
     res.render('index')
 })
+
+
+
+
+
+
 
 
 app.listen(8000,'localhost')
