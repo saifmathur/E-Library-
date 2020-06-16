6e130613946dda15d80342634270734141b6ae76
saifmathur <saifmathur41@gmail.com>
2020-06-17 04:18:15 +0530

    added routes for index and contact

--- controller/routes.js
+++ controller/routes.js
@@ -1,3 +1,23 @@
 const express = require('express')
 const router = express.Router()
 
+//getting the database in
+const Store = require('../models/store')
+
+const { query } = require('express');
+
+router.use(bodyParser.urlencoded(
+    { extended: false }
+))
+router.use(bodyParser.json());
+
+router.get('/', function(req,res){
+    res.render('index')
+})
+
+router.get('/contact', function(req,res){
+    res.render('contact')
+})
+
+
+
