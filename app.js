18c91f2f1a28540832d74d43a5f9dc1eec50959f
saifmathur <saifmathur41@gmail.com>
2020-06-16 13:23:14 +0530

    layout added

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+app = express()
+
+app.set('view engine', 'pug')
+app.set('views', __dirname + '/views')
+
+
+
+app.use('/', function(req,res){
+    res.render('index')
+})
+
+
+app.listen(8000,'localhost')
