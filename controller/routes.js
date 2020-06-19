38826cbe7fd59798b8dc12cbef0d3adb328f8808
saifmathur <saifmathur41@gmail.com>
2020-06-19 19:54:30 +0530

    resolved: found a way to search all collections

--- controller/routes.js
+++ controller/routes.js
@@ -14,6 +14,7 @@ const mongoose = require('mongoose')
 
 //getting the database in
 const Library = require('../models/store')
+const allCollections = Library.allCollections
 const { query } = require('express');
 
 
@@ -28,9 +29,14 @@ router.get('/', function(req,res){
     res.render('index',{
       search: req.body.search  
     })
-      
-})
-  
+    Library.FindAllBooks().forEach(model=>{
+        model.find({},function(err,result){
+            console.log(result)
+        })
+    })
+    
+
+}) 
 
 router.get('/contact', function(req,res){
     res.render('contact')
@@ -42,7 +48,15 @@ router.get('/category', function(req,res){
     res.render('category',{
         categories: Library.allCollections
     })
-    console.log(Library.allCollections)
+    //console.log(Library.allCollections)
+})
+
+router.get('/category/:category', function(req, res){
+    console.log(query)
+    res.render('category',{
+    
+    
+    })
 })
 
 module.exports = router;
--- models/store.js
+++ models/store.js
@@ -5,7 +5,7 @@ const MongoClient = mongodb.MongoClient
 
 
 const allCollections = []
-const allResults = []
+
 MongoClient.connect('mongodb://localhost:27017/BookStore',function(err,client){
     if(err){
         console.log(err)    
@@ -19,6 +19,7 @@ MongoClient.connect('mongodb://localhost:27017/BookStore',function(err,client){
             else{
                 collections.forEach(eachCollectionDetails => {
                     allCollections.push(eachCollectionDetails.name)
+
                     //console.log(eachCollectionDetails.name)
                 })
             }
@@ -37,36 +38,48 @@ MongoClient.connect('mongodb://localhost:27017/BookStore',function(err,client){
 })
 
 
-
-
-
-
-
-const categoryInspiration = new mongoose.Schema({
-    name:{type: String, required: true},
-    genre:{type:String, required: false},
-    ISBN:{type: String, required: false},
-    author:{type:String, required: false},
-    //File:{type:Image, required: false}
-    })
-
-
-const categoryFiction = new mongoose.Schema({
+const categorySchema = new mongoose.Schema({
     name:{type: String, required: true},
     genre:{type:String, required: false},
     ISBN:{type: String, required: false},
     author:{type:String, required: false},
     //File:{type:Image, required: false}
     });
+//searching all collections
+const FindAllBooks = function(){
+    let models = []
+    models.push(mongoose.models.Fantasy)
+    models.push(mongoose.models.Fiction)
+    models.push(mongoose.models.Horror)
+    models.push(mongoose.models.Inspirational)
+    models.push(mongoose.models.Romance)
+    models.push(mongoose.models.SciFi)
+    models.push(mongoose.models.Thriller)
+    models.push(mongoose.models.store)
+    return models
+}
 
-
-
-const Inspirational =  mongoose.model('Inspirational',categoryInspiration,'Inspirational')
-const Fiction = mongoose.model('Fiction', categoryFiction,'Fiction')
+const Inspirational =  mongoose.model('Inspirational',categorySchema,'Inspirational')
+const Fiction = mongoose.model('Fiction', categorySchema,'Fiction')
+const Thriller = mongoose.model('Thriller',categorySchema,'Thriller')
+const SciFi = mongoose.model('SciFi',categorySchema,'Sci-Fi')
+const Horror = mongoose.model('Horror',categorySchema,'Horror')
+const Fantasy = mongoose.model('Fantasy',categorySchema,'Fantasy')
+const Romance = mongoose.model('Romance',categorySchema,'Romance')
+const store = mongoose.model('store',categorySchema,'store')
 
 
 module.exports = {
     Inspirational,
     Fiction,
-    allCollections
+    allCollections,
+    Thriller,
+    SciFi,  
+    Fiction,
+    Horror,
+    Inspirational,
+    Fantasy,
+    Romance,
+    store,
+    FindAllBooks
 }
